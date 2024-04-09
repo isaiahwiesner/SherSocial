a1ebb9f253602a9ed0a64d5b53faa93fb6dc18b9
izzzzthewiz <isaiah.wiesner@yahoo.com>
2024-04-09 09:00:41 -0400

    Fixed home page

--- static/js/pages/home.js
+++ static/js/pages/home.js
@@ -31,7 +31,7 @@ const appendPosts = (posts) => {
                         <h4 class="mb-2">${p.title}</h4>
                         <section class="d-flex gap-3 align-items-center mb-2">
                             ${p.creatorImage
-                        ? `<img src="${p.creatorImage}" class="rounded-circle avatar object-fit-cover" alt="${p.user.fullName}">`
+                        ? `<img src="${p.creatorImage}" class="rounded-circle avatar object-fit-cover" alt="${p.creatorFullName}">`
                         : `<svg xmlns="http://www.w3.org/2000/svg" class="rounded-circle avatar" viewBox="0 0 448 512">
                                 <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                                 <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
@@ -53,7 +53,7 @@ const appendPosts = (posts) => {
                         <h4 class="mb-2">${p.title}</h4>
                         <section class="d-flex gap-3 align-items-center mb-2">
                             ${p.creatorImage
-                        ? `<img src="${p.creatorImage}" class="rounded-circle avatar object-fit-cover" alt="${p.user.fullName}">`
+                        ? `<img src="${p.creatorImage}" class="rounded-circle avatar object-fit-cover" alt="${p.creatorFullName}">`
                         : `<svg xmlns="http://www.w3.org/2000/svg" class="rounded-circle avatar" viewBox="0 0 448 512">
                                 <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                                 <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
